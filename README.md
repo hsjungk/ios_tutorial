24ad769395b575d861eaaca7d1f547c80bb03562
hsjung <jung3354@gmail.com>
2023-09-26 18:27:57 +0900

    4. Supplement accessibility data

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ https://delicate-walrus-052.notion.site/1-Create-a-project-992691e1258143c18c1cb
 
 ## 2. Compose views
 https://delicate-walrus-052.notion.site/2-Compose-views-7ab25940e0b6441ba2f38cc9f616b8bd?pvs=4
+
+## 3. Modify and style views
+https://delicate-walrus-052.notion.site/3-Modify-and-style-views-60bd9f99deb44d8890de21526affe09a?pvs=4
+
+## 4. Supplement accessibility data
+https://delicate-walrus-052.notion.site/4-Supplement-accessibility-data-e3bc0bfdd16b451aa6a9ebc86a1d9308?pvs=4
--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -24,6 +24,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
             HStack {
@@ -31,6 +34,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
